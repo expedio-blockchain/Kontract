fc641ea9b845139329395d7cd71b1676df58bb11
ron34353435 <ron34353435@gmail.com>
2024-08-25 21:59:02 +0300

    wallet working both create new and import

--- kubebuilder/go.mod
+++ kubebuilder/go.mod
@@ -14,11 +14,19 @@ require (
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bits-and-blooms/bitset v1.10.0 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.4 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-ethereum v1.14.8 // indirect
 	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
@@ -40,11 +48,13 @@ require (
 	github.com/google/pprof v0.0.0-20240525223248-4bfdf5a9a2af // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
+	github.com/holiman/uint256 v1.3.1 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
@@ -56,6 +66,7 @@ require (
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stoewer/go-strcase v1.2.0 // indirect
+	github.com/supranational/blst v0.3.11 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.53.0 // indirect
 	go.opentelemetry.io/otel v1.28.0 // indirect
@@ -67,14 +78,15 @@ require (
 	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
-	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/term v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240528184218-531527333157 // indirect
@@ -91,6 +103,7 @@ require (
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.30.3 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
--- kubebuilder/internal/controller/wallet_controller.go
+++ kubebuilder/internal/controller/wallet_controller.go
@@ -18,10 +18,16 @@ package controller
 
 import (
 	"context"
+	"encoding/hex"
+	"fmt"
 
+	"github.com/ethereum/go-ethereum/crypto"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	kontractdeployerv1alpha1 "github.com/expedio-blockchain/KontractDeployer/api/v1alpha1"
@@ -36,20 +42,106 @@ type WalletReconciler struct {
 // +kubebuilder:rbac:groups=kontractdeployer.expedio.xyz,resources=wallets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=kontractdeployer.expedio.xyz,resources=wallets/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=kontractdeployer.expedio.xyz,resources=wallets/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=create;update;get;list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// Reconcile is part of the main Kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Wallet object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *WalletReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Fetch the Wallet instance
+	wallet := &kontractdeployerv1alpha1.Wallet{}
+	err := r.Get(ctx, req.NamespacedName, wallet)
+	if err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Check if the wallet is already created
+	if wallet.Status.PublicKey != "" && wallet.Status.SecretRef != "" {
+		logger.Info("Wallet already created", "PublicKey", wallet.Status.PublicKey)
+		return ctrl.Result{}, nil
+	}
+
+	var secretName string
+	if wallet.Spec.ImportFrom != nil && wallet.Spec.ImportFrom.SecretRef != "" {
+		// Use the provided secretRef
+		secretName = wallet.Spec.ImportFrom.SecretRef
+		logger.Info("Using existing secret", "SecretRef", secretName)
+
+		// Fetch the Secret to extract the public key
+		existingSecret := &corev1.Secret{}
+		err := r.Get(ctx, client.ObjectKey{Name: secretName, Namespace: req.Namespace}, existingSecret)
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to fetch existing secret: %v", err)
+		}
+
+		// Extract the public key from the Secret
+		publicKey, exists := existingSecret.Data["publicKey"]
+		if !exists {
+			return ctrl.Result{}, fmt.Errorf("publicKey not found in the secret: %s", secretName)
+		}
+
+		// Update the Wallet status with the public key and secretRef
+		wallet.Status.PublicKey = string(publicKey)
+		wallet.Status.SecretRef = secretName
+		err = r.Status().Update(ctx, wallet)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		logger.Info("Wallet imported", "PublicKey", wallet.Status.PublicKey, "SecretRef", wallet.Status.SecretRef)
+
+	} else {
+		// Generate a new Ethereum wallet
+		privateKey, err := crypto.GenerateKey()
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to generate wallet: %v", err)
+		}
+
+		publicKey := crypto.PubkeyToAddress(privateKey.PublicKey).Hex()
+		privateKeyHex := hex.EncodeToString(crypto.FromECDSA(privateKey))
+
+		// Create a new Kubernetes Secret to store the wallet keys
+		secretName = fmt.Sprintf("%s-wallet-secret", wallet.Name)
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      secretName,
+				Namespace: req.Namespace,
+			},
+			StringData: map[string]string{
+				"privateKey": privateKeyHex,
+				"publicKey":  publicKey,
+			},
+		}
+
+		// Set the Wallet instance as the owner of the Secret
+		if err := controllerutil.SetControllerReference(wallet, secret, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// Check if the Secret already exists
+		found := &corev1.Secret{}
+		err = r.Get(ctx, client.ObjectKey{Name: secret.Name, Namespace: secret.Namespace}, found)
+		if err != nil && client.IgnoreNotFound(err) == nil {
+			// Create the Secret
+			err = r.Create(ctx, secret)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			logger.Info("Secret created", "Secret.Name", secret.Name)
+		} else if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// Update the Wallet status with the public key and secretRef
+		wallet.Status.PublicKey = publicKey
+		wallet.Status.SecretRef = secretName
+		err = r.Status().Update(ctx, wallet)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		logger.Info("Wallet created", "PublicKey", publicKey, "SecretRef", secretName)
+	}
 
 	return ctrl.Result{}, nil
 }
