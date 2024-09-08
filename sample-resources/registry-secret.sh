b2fd894a3e479e45708b419512b1fc319d71c8c3
ron34353435 <ron34353435@gmail.com>
2024-09-08 21:53:34 +0300

    improve contract verification

--- sample-resources/registry-secret.sh
+++ sample-resources/registry-secret.sh
@@ -3,3 +3,5 @@ kubectl create secret -n kubebuilder-system docker-registry regcred \
   --docker-username=expedio \
   --docker-password=dckr_pat_w3ifoB9MxAQSuhxO433GfrGtmZE \
   --docker-email=ronmegini@expedio.xyz
+
+kubectl patch -n kubebuilder-system serviceaccount kubebuilder-controller-manager -p '{"imagePullSecrets": [{"name": "regcred"}]}'
